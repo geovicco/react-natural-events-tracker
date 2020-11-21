57fef944412bfa1c898ad146f983b3ccdae35894
geovicco <71774828+geovicco@users.noreply.github.com>
2020-11-21 11:50:25 +0530

    Updated App.js to Fetch API data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react';
 import Map from './components/Map';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+// Fetch Data from NASA's EONET API
+
 function App() {
+  
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      
+      setLoading(true)
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
+      const { events } = await res.json()
+      setEventData(events)
+      setLoading(false)
+    }
+    fetchEvents()
+    console.log(eventData);
+  }, [eventData])
+
   return (
     <div>
       <Map/>
